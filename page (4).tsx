37517146426d6c0e1082aa0c9204bfc6f4531a00
bookreadplease8888-cpu <book.read.please.8888@gmail.com>
2026-03-31 22:03:11 +0900

    Delete page (4).tsx

--- page (4).tsx
+++
@@ -1,8 +0,0 @@
-import type { MetadataRoute } from "next";
-import { STYLE_MAP } from "@/lib/constants";
-import { siteConfig } from "@/lib/site";
-
-export default function sitemap(): MetadataRoute.Sitemap {
-  const resultPages = Object.keys(STYLE_MAP).map((style) => ({ url: `${siteConfig.url}/result/${style}`, lastModified: new Date() }));
-  return [{ url: siteConfig.url, lastModified: new Date() }, ...resultPages];
-}
